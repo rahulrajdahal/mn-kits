9deb2e5a86687833982ac55c31c60d782bb506b9
rahulrajdahal <rahulrajdahal@gmail.com>
2023-09-06 23:52:46 +0545

    fix(versioning): initial

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: path.resolve("src", "main.ts"),
